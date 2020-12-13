dfd7772c8cd30fc018fb33303f35a9989112d190
Joe Alongi <collectedview@gmail.com>
2020-12-13 16:51:09 -0500

    Performance Optimizations
    React Memo performance optimizations.
    
    Remove Instructions
    
    Remove instructions from project source.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useReducer, useEffect, useCallback} from 'react';
+import React, {useReducer, useEffect, useCallback, useMemo} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -47,7 +47,7 @@ const Ingredients = () => {
       ingredients: filteredIngredients});
   }, []);
 
-  const addIngredientHandler = ingredient => {
+  const addIngredientHandler = useCallback(ingredient => {
     dispatchHttp({type: 'SEND'});
     fetch('https://react-hooks-update-62181.firebaseio.com/ingredients.json', {
       method: 'POST',
@@ -63,9 +63,9 @@ const Ingredients = () => {
       type:'ADD', 
       ingredient: {id: responseData.name, ...ingredient}});
     });
-  };
+  }, []);
 
-  const removeIngredientHandler = ingredientId => {
+  const removeIngredientHandler = useCallback (ingredientId => {
     dispatchHttp({type: 'SEND'});
     fetch(
       `https://react-hooks-update-62181.firebaseio.com/ingredients/${ingredientId}.json`, 
@@ -80,12 +80,20 @@ const Ingredients = () => {
     }).catch(error => {
       dispatchHttp({type: 'ERROR', errorMessage: 'Something went wrong!' });
     });
-  };
+  }, []);
 
-  const clearError = ( ) => {
+  const clearError = useCallback(() => {
     dispatchHttp({type: 'CLEAR'});
-  }
+  }, []);
 
+  const ingredientList = useMemo(() => {
+    return (
+      <IngredientList 
+        ingredients={userIngredients} 
+        onRemoveItem={removeIngredientHandler}
+      />
+    )
+  }, [userIngredients, removeIngredientHandler]);
   
   return (
     <div className="App">
@@ -98,7 +106,7 @@ const Ingredients = () => {
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+        {ingredientList}
       </section>
     </div>
   );
